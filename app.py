a96c69504667cec274da43cc965b6aede7341570
Amine Bozan <aminebozan736@gmail.com>
2026-06-26 22:18:38 +0300

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -205,5 +205,5 @@ def track():
     finally:
         conn.close()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
